db62da3cdc28a003c2f0e0034f3ef1ed4411697a
Peter Higgins <pah@cenabo.com>
2018-05-08 13:23:22 +0100

    Add user accounts

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,13 +3,18 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 import ResolutionForm from './ResolutionForm';
+import RegisterForm from './RegisterForm';
+import LoginForm from './LoginForm';
 
 const App = ({ loading, resolutions }) => {
   if (loading) return null;
 
   return (
     <div>
+      <RegisterForm />
+      <LoginForm />
       <ResolutionForm />
+      <button onClick={() => Meteor.logout()}>Logout</button>
       <ul>
         {resolutions.map(resolution => (
           <li key={resolution._id}>{resolution.name}</li>
---
+++ imports/ui/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Accounts } from 'meteor/accounts-base';
+
+class LoginForm extends Component {
+  loginUser = e => {
+    e.preventDefault();
+
+    Meteor.loginWithPassword(this.email.value, this.password.value, error => {
+      console.log(error);
+    });
+  }
+  render() {
+    return (
+      <form onSubmit={this.loginUser}>
+        <input type="email" ref={input => this.email = input} />
+        <input type="password" ref={input => this.password = input} />
+        <button type="submit">Login User</button>
+      </form>
+    )
+  }
+};
+
+export default LoginForm;
